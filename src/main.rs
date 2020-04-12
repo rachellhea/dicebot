aa15ce7bb0b3d628db9de93cff7dc774bf407fb2
Rachel Forshee <rachel.i.forshee@gmail.com>
2020-04-11 19:49:00 -0700

    Ignore bot's own output in newly received messages

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use discord::model::Event;
+use discord::model::UserId;
 use discord::Discord;
 use rand::Rng;
 use std::env;
@@ -6,17 +7,25 @@ use std::env;
 fn main() {
     // Log into Discord using the bot token.
     let token = &env::var("DISCORD_TOKEN").expect("Expected token");
-    let discord = Discord::from_bot_token(token)
-        .expect("login failed");
+    let bot_id = &env::var("DISCORD_USER_ID").expect("Expected bot user ID");
+    let bot_id = UserId(bot_id.parse::<u64>().unwrap());
 
+    let discord = Discord::from_bot_token(token).expect("login failed");
     let (mut connection, _) = discord.connect().expect("connect failed");
     println!("Ready!");
 
     loop {
         match connection.recv_event() {
             Ok(Event::MessageCreate(message)) => {
-                println!("[NEW MSG] {} - {}", message.author.name, message.content);
+                // Ignore the bot's own messages to prevent log pollution
+                if message.author.id == bot_id {
+                    continue;
+                }
 
+                println!("[DEBUG] {} (user ID: {}) sent message: {}",
+                        message.author.name,
+                        message.author.id,
+                        message.content);
                 if message.content.starts_with("!random")
                         || message.content.starts_with("!roll") {
                     let rand = rand::thread_rng().gen_range(1, 1001);
@@ -24,7 +33,7 @@ fn main() {
                             message.author.name,
                             rand);
 
-                    println!("{}", out);
+                    println!("[INFO] {}", out);
                     let _ = discord.send_message(message.channel_id,
                             &out,
                             "",
